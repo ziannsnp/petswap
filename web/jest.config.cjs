f14e51d23ec38924ff418d6a0f2c7977786f03e5
Sornchon Srichoonim <sornchon.srichoonim@gmail.com>
2026-09-01 04:43:13 +0700

    test(bookings): cover outgoing/incoming visibility and BookingsScreen UI states

--- web/jest.config.cjs
+++ web/jest.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.app.json' }],
+  },
 };

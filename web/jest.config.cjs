561c389b09981fe208a973f1ed45cdb3041a295d
pw'ice <146358111+pwaii@users.noreply.github.com>
2026-09-06 12:56:53 +0700

    Update jest.config.cjs
    resolved configuration conflict

--- web/jest.config.cjs
+++ web/jest.config.cjs
@@ -5,6 +5,6 @@ module.exports = {
   testMatch: ['<rootDir>/src/**/*.test.ts'],
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.app.json' }],
   },
 };
